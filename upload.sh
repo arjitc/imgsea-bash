bbd33d70620e7ed6b77a21c40973e4dc104d393f
Arjit Chaudhary <arjit.c@gmail.com>
2013-07-10 18:58:38 +0530

    Works!

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash          
 echo "Enter the image name incl. extention "
 read input_variable
-$upload=curl -F "file=@$input_variable" http://imgsea.com/beta/upload.php
 echo
-echo $upload
+curl -F "file=@$input_variable" http://imgsea.com/beta/upload.php
+echo
